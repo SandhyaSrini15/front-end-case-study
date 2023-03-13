cce5bb8624a5f51ed9945d899ee3126db0e14673
Sandhya Srinivasan <sandy150225@gmail.com>
2023-03-13 13:21:21 +0100

    added dynamic link routing

--- app/components/Dropdown.jsx
+++ app/components/Dropdown.jsx
@@ -2,7 +2,6 @@ import Link from "next/link";
 import React, { useState, useEffect } from "react";
 
 function Dropdown({ dropdownList, parentName, onClickFn }) {
-
   return (
     dropdownList.length > 0 && (
       <div className={`px-10 border-blue-700 border-1px hidden ${parentName}`}>
@@ -10,14 +9,18 @@ function Dropdown({ dropdownList, parentName, onClickFn }) {
           {dropdownList.map((el) => {
             return (
               <div key={el.id}>
-                <Link href="#" onClick={onClickFn}>
+                <Link
+                  href={el.children.length > 0 ? "#" : `/Link/${el.label}`}
+                  onClick={onClickFn}
+                >
                   {el.label}
                 </Link>
                 {el.children && el.children.length > 0 && (
                   <Dropdown
                     dropdownList={el.children}
                     parentName={el.label}
-                    onClickFn={onClickFn}                  />
+                    onClickFn={onClickFn}
+                  />
                 )}
               </div>
             );
--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -23,18 +23,20 @@ function Navbar({ data }) {
   return (
     <div>
       <div>
-        <nav className="bg-white border-gray-400 rounded justify-end">
-          <ul className="flex flex-col justify-end md:flex-row md:space-x-8">
+        <nav className="px-2 bg-white border-gray-200 dark:bg-gray-900 dark:border-gray-700">
+          <ul className="flex flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
             {state.map((element) => {
               return (
                 <div key={element.id}>
+                  <li>
                   <Link
-                    href="#"
+                    href={element.children.length > 0 ? '#' : `/Link/${element.label}`}
                     className="block py-2 pl-3 pr-4 rounded md:bg-transparent text-blue-700"
                     onClick={handleClick}
                   >
                     {element.label}{" "}
                   </Link>
+                  </li>
                   {element.children.length > 0 && (
                     <Dropdown
                       dropdownList={element.children}
